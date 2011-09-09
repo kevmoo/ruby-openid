871784b6fed3da998e42a25e2a59f1d670c016da
Tom Quackenbush <tquackenbush@agoragames.com>
2011-09-09 13:15:10 -0400

    add Util::HTML_FORM_ID constant for use in generated auto-submit openid transaction in progress form to be sure we are autosubmitting the correct form

--- lib/openid/util.rb
+++ lib/openid/util.rb
@@ -26,6 +26,8 @@ module OpenID
      [#{BASE64_CHARS}]{3}=)?
     \\Z", Regexp::EXTENDED)
 
+    HTML_FORM_ID = 'openid_transaction_in_progress'
+
     def Util.assert(value, message=nil)
       if not value
         raise AssertionError, message or value
@@ -91,9 +93,9 @@ module OpenID
                   <style>form { visibility: hidden }</style>
                   <script>
                     var server_proceed = setTimeout(function() {
-                      if (typeof document.forms[0] == 'object') {
+                      if (typeof document.getElementById('#{HTML_FORM_ID}') == 'object') {
                         clearTimeout(server_proceed);
-                        document.forms[0].submit();
+                        document.getElementById('#{HTML_FORM_ID}').submit();
                       }
                     }, 100);
                   </script>
